20ddd608188e68074287d98edb337e882914fc38
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-05 01:52:21 +0530

    take command function added to take voice as input and convert to text

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,6 @@
 import pyttsx3
 import datetime
+import speachRecognition as sr
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[1].id)
@@ -9,15 +10,24 @@ def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
+
 def WishMe():
     hour = int(datetime.datetime.now().hour)
-    if hour>= 0 and hour <12:
+    if hour >= 0 and hour < 12:
         speak("Good Afternoon!")
-    elif hour>=12 and hour < 18:
+    elif hour >= 12 and hour < 18:
         speak("Good Afternoon!")
     else:
         speak("Good Evening!")
     speak("Hey, I'm Jarvis, how can i help you")
+
+
+def takeCommand():
+    r = sr.Rcongnizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshould = 1
+
 
 if __name__ == "__main__":
     WishMe()
