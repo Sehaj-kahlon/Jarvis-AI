9c60d31f156b5da92a541c5f4e99fdcf9c50d0cc
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-05 02:22:25 +0530

    converted voice to text

--- jarvis.py
+++ jarvis.py
@@ -1,6 +1,6 @@
 import pyttsx3
 import datetime
-import speachRecognition as sr
+import speech_recognition as sr
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 engine.setProperty('voices', voices[1].id)
@@ -14,7 +14,7 @@ def speak(audio):
 def WishMe():
     hour = int(datetime.datetime.now().hour)
     if hour >= 0 and hour < 12:
-        speak("Good Afternoon!")
+        speak("Good Morning!")
     elif hour >= 12 and hour < 18:
         speak("Good Afternoon!")
     else:
@@ -23,11 +23,22 @@ def WishMe():
 
 
 def takeCommand():
-    r = sr.Rcongnizer()
+    r = sr.Recognizer()
     with sr.Microphone() as source:
         print("Listening...")
         r.pause_threshould = 1
+        audio = r.listen(source)
+    
+    try: 
+        print("Recognizing...")
+        query = r.recognize_google(audio, language  ='en-in')
+        print(f"User said: {query}\n")
+    except Exception as e:
+        print("Sorry, I did'nt get that. Please try saying again")
+        return "None"
+    return query
 
 
 if __name__ == "__main__":
     WishMe()
+    query = takeCommand().lower()
